0d551bb2495954f0c26f2c555f486980323b7b7e
pavel.zehnula <pavel.zehnula@olc.cz>
2021-05-19 09:29:47 +0200

    Test pull!

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/ucitele')
+def hello_teacher():
+    return 'Hi World!'
+
 
 if __name__ == '__main__':
     app.run()
