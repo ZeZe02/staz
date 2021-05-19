72ebcc92e14446568d2431d99baa8efb6eaa571d
Marrek Nožka <marek@tlapicka.net>
2021-05-19 09:20:52 +0200

    startujeme

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return "Hello World!"
 
 
-if __name__ == '__main__':
-    app.run()
+@app.get("/marek/")
+def marek():
+    return render_template("base.html")
+
+
+@app.post("/marek/")
+def marek_post():
+    return render_template("base.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
