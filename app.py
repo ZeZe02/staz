277ca9e9699ece8c280e08a189ca8690b1e0c616
VaGithub1 <vakri@spseol.cz>
2021-05-20 13:21:08 +0200

    add teacher form - funkcni route: /ucitel/

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
+from pony.flask import Pony
+from pony.orm import commit, db_session
+#import models
+from models import *
 
 app = Flask(__name__)
+app.secret_key = "asfuogasdhkjfbvasdhjkcbvdasjhk,56789"
+Pony(app)
 
 
+
+@db_session
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    """
+    u = Teacher(name="Igor",login="igor")
+    commit()  # asi zde není potřeba, protože ten dekorátor db_session zajistí, že celá tato funkce bude brána jako jedna transakce a commitne se automaticky na konci funkce
+    """
+    jmeno = Teacher.get(id=1).name   # tohle ještě nezpůsobí SQL dotaz do DB, ale nachystá si to a až při použití té proměnné jmeno ten dotaz proběhne
+    pracejednicky = Teacher.get(id=1).projects
+
+    return render_template("index.html",jmeno=jmeno,pracejednicky=pracejednicky)
 
 @app.route('/base/')
 def muj_base():
@@ -23,6 +38,23 @@ def ucitele():
 def ucitele_name(name):
     return f'Hello, {name}!'
 
+
+
+
+@app.route("/ucitel/", methods = ["GET", "POST"])
+def ucitel():
+    if request.method == "POST":
+        login = request.form.get("login")
+        jmeno = request.form.get("fname")
+        prijm = request.form.get("lname")
+        manager = request.form.get("manager")
+        fullname = f"{jmeno} {prijm}"
+        t = create_teacher(login=login, name=fullname, manager=manager)
+        flash(f"Úspěšně přidán učitel {fullname} manager={manager}")  # nachystá do zásobníku (v rámci cookies) tuto "hlášku" a šablona base si ji pak použije
+        # v ramci requestu se da detekovat i mobil - request z mobilniho telefonu
+        # dat sem breakpoint a pak inspekovat request
+        return redirect(url_for("ucitel"))
+    return render_template("teacher.html")
 
 if __name__ == '__main__':
     app.run()
