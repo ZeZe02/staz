b1098e87d743358a647309953ed0817496332e91
Radka19 <gebova@spseol.cz>
2021-05-20 13:30:57 +0200

    přidání učitele

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+from pony.flask import Pony
+from models import *
+
 
 app = Flask(__name__)
+app.secret_key = "safafafaijwdugwzrhofbweuhfkbewbowfv"
+Pony(app)
+
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html", pozdrav="Ahoj", cisla=range(0,10), zobraz=True)
+
+@app.route("/ucitel/", methods=["GET","POST"])
+def teacher():
+    if request.method == "POST":
+        fname = request.form.get("fname", "")
+        lname = request.form.get("lname", "")
+        fullname = f"{fname} {lname}"
+        login = request.form.get("login", "")
+        manager = request.form.get("manager", "")
+        create_teacher(fullname, login, manager)
+        flash("Vytvořeno!!")
+        return redirect(url_for("teacher"))
+    else:
+        return render_template("teacher.html")
+
+
+@app.route("/ucitele/")
+def pozdrav_ucitel():
+    return render_template("ucitele.html")
+
+@app.route('/ucitele/<name>/')
+def krasny_den(name):
+    return 'Krásný den ' + str(name)
 
 
 if __name__ == '__main__':
