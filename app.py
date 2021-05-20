f54cb8a4b91f934ec27da60884fa8a6c0a11ac64
Jaroslav Krbec <jaroslavkrbec@google.com>
2021-05-20 13:23:39 +0200

    ctvrtek

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+from pony.flask import Pony
+import models
 
 app = Flask(__name__)
+app.secret_key = "safafafaijwdugwzrhofbweuhfkbewbowfv"
+Pony(app)
+
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html", pozdrav="Ahoj", cisla = range(0,9), zobraz = True)
 
-@app.route('/ucitele')
-def hi_world():
-    return "Hi World!"
+@app.route('/ucitel/', methods=['GET', 'POST'])
+def teacher(login=None):
+    msg = ""
+    if request.method == 'POST':
+        login = request.form.get('login', '')
+        fname = request.form.get('fname', '')
+        lname = request.form.get('lname', '')
+        name = f'{fname} {lname}'
+        manager = request.form.get("manager", '')
+        teacher = models.create_teacher(fname=fname, lname=lname, login=login, manager=manager)
+        flash("Vytvořeno!")
+        return redirect(url_for("teacher"))
+    else:
+        return  render_template('teacher.html')
+
+@app.route('/ucitele/')
+def hello_teacher():
+    return render_template("ucitele.html")
+
+@app.route('/ucitele/<name>')
+def hello_teacher_name(name):
+    return f"Yellow teacher {name}!"
 
 
 
