535698bb3d2acac8a681031bbd0f4c909b777347
pavel.zehnula <pavel.zehnula@olc.cz>
2021-05-21 07:58:47 +0200

    list of teachers

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, flash, redirect, url_for
+from pony.flask import Pony
+from pony.orm import commit, db_session, select
+
+import models
+from managers import TeacherManager
 
 app = Flask(__name__)
+app.secret_key = "safafafaijwdugwzrhofbweuhfkbewbowfv"
+Pony(app)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def homepage():
+    return render_template('pages/homepage/index.html')
+
+
+@app.route('/pridat-ucitel/', methods=['GET', 'POST'])
+def teacher():
+    if request.method == 'POST':
+        fname = request.form.get('fname', '')
+        lname = request.form.get('lname', '')
+        login = request.form.get('login', '')
+        t = TeacherManager.create_teacher(fname, lname, login)
+        flash('Vytvořeno !!')
+        return redirect(url_for('teacher'))
+    return render_template('pages/pridat-ucitele/teacher.html')
 
 
 @app.route('/ucitele/')
-def hello_teacher():
-    return render_template('ucitele.html')
-
-
-@app.route('/ucitele/<name>/')
-def hello_teacher_name(name):
-    return "HI " + str(name)
-
-
+def teachers():
+    list_of_teachers = TeacherManager.get_teachers()
+    return render_template('pages/ucitele/list_of_teachers.html',
+                           teachers=list_of_teachers)
 
 
 if __name__ == '__main__':
